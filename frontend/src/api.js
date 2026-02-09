a0b80f2916d23343dcbe54f0c861dc7d75b27339
Harsh Hadiyal <harshhadiyal7733@gmail.com>
2026-02-09 12:05:30 +0530

    update server

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', 
+  baseURL: 'http://https://resource-management-system-backend.onrender.com/api', 
 });
 
 // THIS IS THE KEY: It attaches the token to every request automatically
